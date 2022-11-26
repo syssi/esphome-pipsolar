4fc60468709068332d6764faeb0594e93c31be02
SeByDocKy <sebydocky@hotmail.com>
2022-11-26 20:19:27 +0100

    Update __init__.py

--- components/pipsolar/binary_sensor/__init__.py
+++ components/pipsolar/binary_sensor/__init__.py
@@ -68,7 +68,7 @@ CONF_WARNING_HIGH_AC_INPUT_DURING_BUS_SOFT_START = (
 )
 CONF_WARNING_BATTERY_EQUALIZATION = "warning_battery_equalization"
 
-# QBATCD sensors
+# QBATCD binary sensors
 
 CONF_DISCHARGE_ONOFF = "discharge_onoff"
 CONF_DISCHARGE_WITH_STANDBY_ONOFF = "discharge_with_standby_onoff
