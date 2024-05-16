fa77e27239d54541bd4a6e05d5fe84c0c36770fc
preussal <>
2024-05-16 11:58:54 +0000

    add Select

--- components/pipsolar/pipsolar.h
+++ components/pipsolar/pipsolar.h
@@ -3,6 +3,8 @@
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
+#include "esphome/components/select/select.h"
+#include "esphome/components/pipsolar/select/pipsolar_select.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/core/automation.h"
