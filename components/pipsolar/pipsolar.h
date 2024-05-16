17042f637bccc173b32a5b8af54002ac6744dc15
preussal <>
2024-05-16 12:44:46 +0000

    test

--- components/pipsolar/pipsolar.h
+++ components/pipsolar/pipsolar.h
@@ -4,7 +4,7 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
 #include "esphome/components/select/select.h"
-#include "esphome/components/pipsolar/select/pipsolar_select.h"
+#include "esphome/components/select/pipsolar_select.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/core/automation.h"
