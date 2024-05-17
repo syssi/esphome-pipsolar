d6b8a3ef7ed0f3f779f9865ee3d4aac9de58caf2
preussal <>
2024-05-17 20:01:12 +0700

    Grummel

--- components/pipsolar/pipsolar.cpp
+++ components/pipsolar/pipsolar.cpp
@@ -167,7 +167,7 @@ void Pipsolar::loop() {
 
         // special for solar power priority select
         if (this->solar_power_priority_) {
-          std::string value = esphome::to_string(value_solar_power_priority__);
+          std::string value = esphome::to_string(value_solar_power_priority_);
           this->solar_power_priority_select_->map_and_publish(value);
         }
 
