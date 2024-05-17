1cdc764ce0b9e64818cc897d624eb06b08d02db7
preussal <>
2024-05-17 19:52:06 +0700

    Add solar power priority Select

--- components/pipsolar/select/__init__.py
+++ components/pipsolar/select/__init__.py
@@ -13,6 +13,7 @@ CONF_STATUSMAP = "statusmap"
 
 CONF_OUTPUT_SOURCE_PRIORITY = "output_source_priority"
 CONF_CHARGER_SOURCE_PRIORITY = "charger_source_priority"
+CONF_SOLAR_POWER_PRIORITY = "solar_power_priority"
 CONF_CHARGING_DISCHARGING_CONTROL = "charging_discharging_control"
 CONF_CURRENT_MAX_CHARGING_CURRENT = "current_max_charging_current"
 CONF_CURRENT_MAX_AC_CHARGING_CURRENT = "current_max_ac_charging_current"
