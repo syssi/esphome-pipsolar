bd84c26b46737ba88fcd98396e52fb85386df4e5
preussal <>
2024-05-18 12:43:31 +0700

    battery_power_direction next try

--- components/pipsolar/pipsolar.h
+++ components/pipsolar/pipsolar.h
@@ -88,7 +88,7 @@ class Pipsolar : public uart::UARTDevice, public PollingComponent {
   PIPSOLAR_BINARY_SENSOR(load_connection, P005GS, int)                    // X
 
   //PIPSOLAR_SENSOR(battery_power_direction, P005GS, int)                   // Y
-  PIPSOLAR_TEXT_SENSOR(battery_power_direction, P005GS, char)      // Y
+  PIPSOLAR_TEXT_SENSOR(battery_power_direction, P005GS)      // Y
   //PIPSOLAR_VALUED_TEXT_SENSOR(battery_power_direction, P005GS, int)      // Y
 
   PIPSOLAR_SENSOR(dc_ac_power_direction, P005GS, int)                     // Z
