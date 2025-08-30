41b6ddcd27d4270c997a2180c914c0d994257558
Rui Rodrigues <rui.rodrigues-ext@alstomgroup.com>
2025-08-30 21:39:43 +0100

    fix convertion on ac_output_voltage select

--- components/pipsolar/pipsolar.cpp
+++ components/pipsolar/pipsolar.cpp
@@ -213,13 +213,14 @@ void Pipsolar::loop() {
         }
          if (this->ac_output_voltage_select_) {          
           if (value_ac_output_voltage_ >= 218 && value_ac_output_voltage_ <= 222) {
-            this->ac_output_voltage_select_->map_and_publish(220);
+            this->ac_output_voltage_select_->map_and_publish(esphome::to_string(220));
           }else if (value_ac_output_voltage_ >= 228 && value_ac_output_voltage_ <= 232) {
-            this->ac_output_voltage_select_->map_and_publish(230);
+            this->ac_output_voltage_select_->map_and_publish(esphome::to_string(230));
           }else if (value_ac_output_voltage_ >= 238 && value_ac_output_voltage_ <= 243) {
-            this->ac_output_voltage_select_->map_and_publish(240);
+            this->ac_output_voltage_select_->map_and_publish(esphome::to_string(240));
           }else{
-            this->ac_output_voltage_select_->map_and_publish(value_ac_output_voltage_select_);
+            std::string value = esphome::to_string(value_output_source_priority_);
+            this->ac_output_voltage_select_->map_and_publish(value);
           }
         }        
         if (this->ac_output_frequency_) {
