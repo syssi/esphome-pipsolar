d0e9c390f9ac8b1f3e7869edad83f30f6d874e03
SeByDocKy <sebydocky@hotmail.com>
2025-11-17 21:39:06 +0100

    Update pipsolar_output.h

--- components/pipsolar/output/pipsolar_output.h
+++ components/pipsolar/output/pipsolar_output.h
@@ -31,7 +31,7 @@ template<typename... Ts> class SetOutputAction : public Action<Ts...> {
   TEMPLATABLE_VALUE(float, level)
 #if ESPHOME_VERSION_CODE >= VERSION_CODE(2025, 12, 0)
     void play(const Ts&... x) override { this->output_->set_value(this->level_.value(x...)); }
-#elif #if ESPHOME_VERSION_CODE < VERSION_CODE(2025, 12, 0)
+#elif ESPHOME_VERSION_CODE < VERSION_CODE(2025, 12, 0)
     void play(Ts... x) override { this->output_->set_value(this->level_.value(x...)); }
 #endif
  protected:
