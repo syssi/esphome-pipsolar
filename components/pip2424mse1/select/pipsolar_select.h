3568c5d155794b35f0a7de61f12b574ec7243528
Sebastian Muszynski <basti@linkt.de>
2026-04-19 19:36:45 +0200

    Fix issue

--- components/pip2424mse1/select/pipsolar_select.h
+++ components/pip2424mse1/select/pipsolar_select.h
@@ -15,8 +15,8 @@ class PipsolarSelect : public Component, public select::Select {
  public:
   void set_parent(Pipsolar *const parent) { this->parent_ = parent; }
   void set_optimistic(bool optimistic) { this->optimistic_ = optimistic; }
-  void add_mapping(std::string key, std::string value) { this->mapping_[key] = value; }
-  void add_status_mapping(std::string key, std::string value) { this->status_mapping_[key] = value; }
+  void add_mapping(const std::string &key, std::string value) { this->mapping_[key] = std::move(value); }
+  void add_status_mapping(const std::string &key, std::string value) { this->status_mapping_[key] = std::move(value); }
   void dump_config() override;
   void control(const std::string &value) override;
   void map_and_publish(std::string &value);
